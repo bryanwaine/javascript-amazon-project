792445c8c4d52d776d748e5e58b66540c2a09f27
bryanwaine <nwanne.ezeaka@gmail.com>
2024-05-26 21:32:57 +0100

    Remove default tests and add currency format tests

---
+++ tests-jasmine/currencyFormatTest.js
@@ -0,0 +1,20 @@
+import { formatCurrency } from "../scripts/utils/currencyFormat.js";
+
+describe("test suite: formatCurrency", () => {
+  it("converts cents to dollars", () => {
+    expect(formatCurrency(1000)).toEqual("10.00");
+  });
+    
+  it("works with zero", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+    
+  it("rounds up to 2 decimal places", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
+    
+  it("rounds down to 2 decimal places", () => {
+    expect(formatCurrency(2000.49)).toEqual("20.00");
+  });
+});
+
