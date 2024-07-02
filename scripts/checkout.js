79b607c8bb20b459507b0af2f9973e62d46198e1
bryanwaine <nwanne.ezeaka@gmail.com>
2024-07-02 13:21:37 +0100

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import renderPaymentSummary from "./checkout/paymentSummary.js";
 import renderOrderSummary from "./checkout/orderSummary.js";
 import renderCheckoutHeader from "./checkout/checkoutHeader.js";
-import './data/car.js'
+import './data/backend-practice.js'
 
 renderPaymentSummary();
 renderOrderSummary();
